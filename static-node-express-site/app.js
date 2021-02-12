1c4c51d40a06b16561163143451eacb25045abad
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-12 15:24:27 -0500

    Update routes

--- static-node-express-site/app.js
+++ static-node-express-site/app.js
@@ -5,18 +5,8 @@ app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/project', (req, res) => {
-    res.render('project');
-});
-
+const routes = require('./routes/index');
+app.use(routes);
 
 
 app.listen(3000);
