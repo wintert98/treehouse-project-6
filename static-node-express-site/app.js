8f8679800446bb5e73d8653842b42b521c123915
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-12 13:32:24 -0500

    Update

--- static-node-express-site/app.js
+++ static-node-express-site/app.js
@@ -2,8 +2,20 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('I Love Treehouse!');
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/project', (req, res) => {
+    res.render('project');
+});
+
+
 
 app.listen(3000);
