6cb3f73e248fc8ef8bff8c8b2d910fd81dc7d28b
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-12 12:57:02 -0500

    Added project files

--- static-node-express-site/app.js
+++ static-node-express-site/app.js
@@ -1,1 +1,9 @@
+const { response } = require('express');
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('I Love Treehouse!');
+});
+
+app.listen(3000);
