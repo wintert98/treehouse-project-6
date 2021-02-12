4b9744e630942e8f30af109746271da71866c236
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-12 14:27:36 -0500

    Update

--- static-node-express-site/app.js
+++ static-node-express-site/app.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
+app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
