8976c32f3e9972a09f0e300871d1901226bfd82f
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-13 11:19:18 -0500

    added images

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/data.json');
+const { projects } = require('../data/data.json');
 
 router.get('/', (req, res) => {
     res.render('index');
