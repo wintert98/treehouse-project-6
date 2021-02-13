4c845991fbc1c39c488610b4ace5b2ab23f1b885
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-13 11:53:22 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { data } = require('../data/data.json');
-const { projects } = require('../data/data.json');
+const { projects } = data;
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -11,8 +11,17 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
-router.get('/project', (req, res) => {
-    res.render('project');
+router.get('/project/:id', (req, res) => {
+    res.render('project', {
+        id: projects[id].id,
+        project_name: projects[id].project_name,
+        description: projects[id].description,
+        technologies: projects[id].technologies,
+        live_link: projects[id].live_link,
+        github_link: projects[id].github_link,
+        image_urls: projects[id].image_urls
+
+    });
 });
 
 module.exports = router;
