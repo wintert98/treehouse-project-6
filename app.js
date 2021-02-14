16aa70282afc6d27f9179e36631f2d73fc5ab745
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-02-14 15:52:21 -0500

    Updated comments

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express();
 app.use('/static', express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'pug');
-
+// Link to routes file
 const routes = require('./routes/index');
 app.use(routes);
-
+// 404 error handler
 app.use((req, res, next) => {
     const err = new Error('Page Not Found');
     err.status = 404;
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
     next(err);
 });
 
-
+// 500 error handler
 app.use((err, req, res, next) => {
     res.locals.error = err;
     const status = err.status || 500;
